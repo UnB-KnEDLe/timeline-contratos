835d1e609f8b2149fabc0e3a019ecbefd2ab87d4
Guilherme Mendes <guimendesp12@gmail.com>
2021-06-01 19:41:15 -0300

    Update db scripts

--- backend/db/ddl.sql
+++ backend/db/ddl.sql
@@ -1,8 +1,22 @@
-CREATE TABLE _ATO(
-    ato_id SERIAL NOT NULL,
-    processo VARCHAR(50),
-    objeto VARCHAR(100),
-    data_dodf VARCHAR(30),
-    texto TEXT,
-    CONSTRAINT _ATO_PK PRIMARY KEY (ato_id)
+CREATE TABLE CERTAME(
+    id_certame SERIAL NOT NULL,
+    processo VARCHAR(30),
+    CONSTRAINT CERTAME_PK primary key (id_certame)
+);
+CREATE TABLE TIPO_ATO(
+    cod_ato SERIAL NOT NULL,
+    descricao VARCHAR(50),
+    CONSTRAINT TIPO_ATO_PK PRIMARY KEY (cod_ato)
+);
+CREATE TABLE ATOS(
+    id_ato SERIAL NOT NULL,
+    data_dodf VARCHAR(15),
+    texto text,
+    id_certame INT,
+    cod_ato INT,
+    CONSTRAINT ATOS_PK PRIMARY KEY (id_ato),
+                FOREIGN KEY(id_certame)
+                 REFERENCES CERTAME(id_certame),
+                 FOREIGN KEY (cod_ato)
+                 REFERENCES TIPO_ATO(cod_ato)
 );
