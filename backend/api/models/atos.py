1cb1a3d6e9aa1eb7398ad8da0bc3271b8ed369ca
Guilherme Mendes <guimendesp12@gmail.com>
2021-06-01 18:31:31 -0300

    Adjust act models

--- backend/api/models/atos.py
+++ backend/api/models/atos.py
@@ -1,15 +1,16 @@
-from sqlalchemy.sql import func
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects import postgresql
 
-from api import db
 
+db = SQLAlchemy()
 
 class AtosModel(db.Model):
     __tablename__ = '_ato'
     ato_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    processo = db.Column(db.String(50), nullable=True)
-    objeto = db.Column(db.String(100), nullable=True)
-    data_dodf = db.Column(db.String(30), nullable=True)
-    texto = db.Column(db.String, nullable=True)
+    processo = db.Column(db.Text, nullable=True)
+    objeto = db.Column(db.Text, nullable=True)
+    data_dodf = db.Column(db.Text, nullable=True)
+    texto = db.Column(db.Text, nullable=True)
 
     def __init__(self, processo, objeto, data_dodf, texto):
         self.processo = processo
