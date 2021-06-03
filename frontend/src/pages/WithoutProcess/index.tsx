b716979d923103d9c11afb29dd0d7d10acca44ad
IanFPFerreira <ianfillipe@gmail.com>
2021-06-03 13:42:22 -0300

    :sparkles: Making process not found page

--- frontend/src/pages/WithoutProcess/index.tsx
+++ frontend/src/pages/WithoutProcess/index.tsx
@@ -1,5 +1,39 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import arrow from '../../assets/arrow.png'
+import logo from '../../assets/logo.svg'
+import imageNotFound from '../../assets/withoutProcess.png'
+
+import { Container, AreaLogo, ImageNotFound, BackButton } from './styles'
+
 
 export const WithoutProcess: React.FC = () => {
-  return <h1>Process</h1>;
+
+  const history = useHistory();
+  const backClick  = () => {
+    history.push('/');
+}
+
+  return (
+    <Container>
+      <BackButton>
+        <img src={ arrow } alt='arrow' onClick={() => backClick()}/>
+      </BackButton>
+      <AreaLogo>
+        <img src={ logo } alt='logo'/>
+      </AreaLogo>
+
+      <ImageNotFound>
+        <img src={ imageNotFound } alt='imageNotFound'/>
+      </ImageNotFound>
+
+      <div>
+        <h1>Processo não encontrado</h1>
+
+        <p>Não foi possível encontrar esse processo, tente novamente.</p>
+      </div>
+
+  </Container>
+  );
 };
--- frontend/src/pages/WithoutProcess/styles.ts
+++ frontend/src/pages/WithoutProcess/styles.ts
@@ -1,3 +1,47 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content:center;
+    align-items:center;
+
+    h1 {
+      font-size: 3rem;
+      font-weight: bold;
+      margin-bottom: 1.5rem;
+      color: var(--primary);
+    }
+    p {
+      font-size: 2rem;
+      max-width: 60rem;
+    }
+  }
+`;
+
+export const BackButton = styled.div`
+    display: flex;
+    position: absolute;
+    width: 100rem;
+    height: 17rem;
+
+`;
+
+export const AreaLogo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 5rem;
+`;
+
+
+export const ImageNotFound = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+padding-left: 5rem;
+
+`;
